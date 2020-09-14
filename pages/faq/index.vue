7b2e987b5a88fcbfe0b9b6ea24d7f7c1f58f88b9
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-14 22:56:01 +0900

    faqリストを実装

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -11,6 +11,42 @@
     >
       <div class="text-center">
         <h1>Welcom to {{ title }} Page</h1>
+        <!-- <v-list-group
+          v-for="(item, i) in faqItems"
+          :key="i"
+          value="true"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-title>{{ item.question }}</v-list-item-title>
+          </template>
+          <v-list-item
+            link
+          >
+            <v-list-item-title>{{ item.answer }}</v-list-item-title>
+          </v-list-item>
+        </v-list-group> -->
+        
+        <v-list>
+          <v-list-group
+            v-for="item in faqItems"
+            :key="item.title"
+            v-model="item.active"
+            no-action
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.question"></v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.answer"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
       </div>
     </v-flex>
   </v-layout>
@@ -20,7 +56,21 @@
 export default {
   data () {
     return {
-      title: 'Q & A'
+      title: 'Q & A',
+      faqItems: [
+        {
+          question: 'Question1',
+          answer: 'Answer1'
+        },
+        {
+          question: 'Question2',
+          answer: 'Answer2'
+        },
+        {
+          question: 'Question3',
+          answer: 'Answer3'
+        }
+      ]
     }
   }
 }
