951ee29a88efc43c51f5f76af6b75a6cfd255192
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-19 06:13:20 +0900

    linksページを実装

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -8,9 +8,44 @@
       xs12
       sm8
       md6
+      style="width: 100%;"
     >
       <div class="text-center">
         <h1>Welcom to {{ title }} Page</h1>
+        <v-list style="margin: 0 auto; max-width: 700px">
+          <v-list-group
+            v-for="(linkGroupItem) in linkGroupItems"
+            :key="linkGroupItem.groupName"
+            no-action
+            sub-group
+            value="true"
+            color="#ffffff"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title
+                  style="text-align: left; white-space: normal; line-height: 1em;"
+                >
+                  {{ linkGroupItem.groupName }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item
+              v-for="(linkItem) in linkGroupItem.linkItems"
+              :key="linkItem.text"
+              style="padding: 0 16px 0 64px;"
+            >
+              <v-list-item-content>
+                <v-list-item-title
+                  style="text-align: left; white-space: normal; line-height: 1em;"
+                >
+                  <a :href="linkItem.link">{{ linkItem.text }}</a>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
       </div>
     </v-flex>
   </v-layout>
@@ -23,7 +58,27 @@
 export default {
   data () {
     return {
-      title: 'Links'
+      title: 'Links',
+      linkGroupItems: [
+        {
+          groupName: 'N高公式',
+          linkItems: [
+            {
+              text: 'N高ホームページ',
+              link: 'https://nnn.ed.jp/'
+            }
+          ]
+        },
+        {
+          groupName: 'N高友好同好会',
+          linkItems: [
+            {
+              text: '写真同好会',
+              link: 'http://syadou.wp.xdomain.jp/'
+            }
+          ]
+        }
+      ]
     }
   }
 }
