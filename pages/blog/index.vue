8e77240812a2700f1690a2e14be4eaf8be646849
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-19 02:33:31 +0900

    about, blog, linksの各ページファイルを作成

---
+++ pages/blog/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <h1>Welcom to {{ title }} Page</h1>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Blog'
+    }
+  }
+}
+</script>
